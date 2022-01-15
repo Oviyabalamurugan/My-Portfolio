d5d23938fdf2440b932a0f19ced444e010bff409
Oviya Balamurugan <55582676+Oviyabalamurugan@users.noreply.github.com>
2022-01-15 22:24:57 +0530

    Initial Commit

---
+++ src/About.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./About.css";
+import aboutImg from "./img/pic.png";
+function About() {
+  //  Up To Top Btn
+    window.addEventListener("scroll", function(){
+      const upToTop = document.querySelector("a.bottom__to__top");
+       upToTop.classList.toggle("active", window.scrollY > 0)
+    });
+  return (
+    <div className="about component__space" id="About">
+      <div className="container">
+        <div className="row">
+          <div className="col__2">
+            <img src={aboutImg} alt="img" height="200" width="200" className="about__img" />
+            <h2>i1 i2 i3 </h2>
+          </div>
+          <div className="col__2">
+            <h1 className="about__heading">About Me</h1>
+            <div className="about__meta">
+              <p className="about__text p__color">
+                I am a Web Development Enthusiast.Currently in my 3rd year Btech degree Amrita Vishwa Vidyapeetham.
+              </p>
+              <p className="about__text p__color">
+                I'm a very determined person.
+              </p>
+              <p className="about__text p__color">
+                
+              </p>
+              <div className="about__button d__flex align__items__center">
+                <a href="#">
+                  <button className="about btn pointer">Download Cv</button>
+                </a>
+                <a href="#">
+                  <button className="about btn pointer">Hire Me</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* UP TO TOP BTN */}
+      <div className="up__to__top__btn">
+        <a href="#" className="bottom__to__top">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-chevron-up white"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+          />
+        </svg>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Project.js
@@ -0,0 +1,128 @@
+import React from "react";
+import "./Project.css";
+import Project1 from './img/IMS.png';
+import Project2 from './img/CL.png';
+import Project3 from './img/BMS.png';
+import Project4 from './img/IMS.png';
+
+function Project() {
+  return (
+    <div className="project component__space" id="Portfolio">
+      <div className="heading">
+        <h1 className="heading">My Project</h1>
+        <p className="heading p__color">
+          There are many variations of passages of Lorem Ipsum available,
+        </p>
+        <p className="heading p__color">
+          but the majority have suffered alteration.
+        </p>
+      </div>
+       <div className="container">
+           <div className="row">
+             
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project1} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h4 className="project__text">Indent Management Website</h4>
+                     <a href="https://indentmanagement.herokuapp.com/" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+
+
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project2} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h4 className="project__text">Amrita Core Lab Website</h4>
+                     <a href="https://indentmanagement.herokuapp.com/dashboard/tasks" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+
+
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project3} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h4 className="project__text">Book Management Website</h4>
+                     <a href="https://oviya06balamurugan.000webhostapp.com/index.php" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+
+
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project4} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Product Recommneder System</h5>
+                     <a href="#" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+
+
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project3} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Safe Shopping Space</h5>
+                     <a href="#" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+
+
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project4} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Spotify Recommender System</h5>
+                     <a href="#" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+             
+             <div className="view__more__btn d__flex align__items__center justify__content__center pxy__30">
+             <button className="view__more pointer btn">View more</button>
+             </div>
+
+           </div>
+       </div>
+    </div>
+  );
+}
+
+export default Project;
